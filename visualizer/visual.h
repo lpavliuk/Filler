e5590e9b4368ff123090931abf64314513ccaaa4
Aleksey Pavliuk <pavliuk.aleksey@gmail.com>
2018-05-24 17:07:14 +0300

    modifier

--- visualizer/visual.h
+++ visualizer/visual.h
@@ -19,12 +19,16 @@
 # define MLX mlx->mlx_ptr
 # define WIN mlx->win_ptr
 # define LINE mlx->line
+# define PLYR_X mlx->player_x
+# define PLYR_O mlx->player_o
 
 typedef struct	s_mlx
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
 	char 	*line;
+	char 	*player_x;
+	char	*player_o;
 }				t_mlx;
 
 void	drawing_net(int i, int j, t_mlx *mlx);
--- visualizer/visualizer.c
+++ visualizer/visualizer.c
@@ -27,6 +27,41 @@ static int exit_x(int keycode)
 	exit (0);
 }
 
+static void	check_input(t_mlx *mlx)
+{
+	int i;
+	int	n;
+
+	i = 0;
+	while (i < 9)
+	{
+		n = 7;
+		get_next_line(0, &LINE);
+		if (ft_strstr(LINE, "launched"))
+		{
+			if (ft_strchr(&LINE[n], '/'))
+			{
+				while (LINE[n] != '/')
+					n++;
+			}
+			if (i > 6)
+				PLYR_X = ft_strdup(&LINE[n + 1]);
+			else
+				PLYR_O = ft_strdup(&LINE[n + 1]);
+		}
+		free(LINE);
+		i++;
+	}
+}
+
+static void	drawing_players(t_mlx *mlx)
+{
+	mlx_string_put(MLX, WIN, 150, 100, 0xFFD700, "Player O: ");
+	mlx_string_put(MLX, WIN, 250, 100, 0xFFD700, PLYR_O);
+	mlx_string_put(MLX, WIN, 150, 150, 0xA52A2A, "Player X: ");
+	mlx_string_put(MLX, WIN, 250, 150, 0xA52A2A, PLYR_X);
+}
+
 int main(void)
 {
 	t_mlx	*mlx;
@@ -37,11 +72,15 @@ int main(void)
 	j = 0;
 	mlx = malloc(sizeof(t_mlx));
 	LINE = NULL;
+	check_input(mlx);
 	get_next_line(0, &LINE);
-	i = atoi(&LINE[0]);
-	j = atoi(&LINE[ft_count(i, 10)]);
+	i = atoi(&LINE[8]);
+	j = atoi(&LINE[8 + ft_count(i, 10)]);
+	free(LINE);
 	MLX = mlx_init();
 	WIN = mlx_new_window(MLX, 1000, 1000, "FILLER");
+	drawing_players(mlx);
+	mlx_string_put(MLX, WIN, 500, 900, 0xFFFFFF, "Press SPACE to start the game!");
 	drawing_net(i, j, mlx);
 	mlx_hook(WIN, 2, 0, key_hook, mlx);
 	mlx_hook(WIN, 17, 1L << 17, exit_x, 0);
